55c8ebd10e58949e36a1409abaeac0c50d9af4e3
darafebriani <darafebriani03@gmail.com>
2021-10-12 14:24:35 +0700

    secound commit

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,51 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/hello', function () {
+    echo "Hello Word";
+});
+
+Route::get('/name/{Dara}', function ($name) {
+    echo "Hello" . $name;
+});
+
+Route::get('/data/{data?}', function ($data = "190313007") {
+    return "Isi Parameter = " . $data;
+});
+
+Route::get('/name/{Dara}', function ($name) {
+    echo "Hello" . $name;
+})->where('Dara', '[A-Za-z]+');
+
+Route::get('/mahasiswa/{nrp}/{nama}', function ($nrp,$nama) {
+    return $nrp ."190313007/Dara Febriani Iskandar  ". $nama;
+});
+
+Route::get('/mahasiswa/{nrp}/{nama}', function ($nrp,$nama) {
+    return $nrp ."190313007/Dara Febriani Iskandar  ". $nama;
+})->where([
+    'nrp'=> '[0-9]+',
+    'nama'=> '[A-Za-z]+'
+]);
+
+Route::get('/cekbilangan/{bilangan}', function ($bilangan) {
+    if($bilangan % 2==0){
+        return "bilangan Genap";
+    }else{
+        return "bilangan Ganjil";
+    }});
+
+    
+Route::get('/deretbilangan/{bilangan}', function ($bilangan) {
+        for ($i = 0; $i <= $bilangan $i++){
+            if($i % 2 == 0){
+                echo "{$i} genap";
+                echo "</br>";
+            }else{
+                echo "{$i} ganjil";
+                echo "</br>";
+            }
+            
+        }
+    });
